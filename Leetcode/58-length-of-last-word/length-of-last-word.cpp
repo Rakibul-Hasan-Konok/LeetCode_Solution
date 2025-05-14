c46ded21e10d7cc2f130d2f3f42c0d90dae0d020
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-05-14 08:43:36 +0600

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.89%) - LeetSync

--- Leetcode/58-length-of-last-word/length-of-last-word.cpp
+++ Leetcode/58-length-of-last-word/length-of-last-word.cpp
@@ -3,13 +3,10 @@ public:
     int lengthOfLastWord(string s) {
         stringstream ss(s);
         string word;
-        string lastWord;
-
         while (ss >> word) {
-            lastWord = word;
         }
 
-        return lastWord.length();
+        return word.length();
     }
     
 };
