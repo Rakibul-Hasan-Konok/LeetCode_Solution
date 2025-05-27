a5172186d011f40ad56f9805af552dca67202677
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-05-27 16:07:26 +0600

    Time: 1 ms (74.69%) | Memory: 40.7 MB (68.69%) - LeetSync

---
+++ Leetcode/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+     int sumN=0,sumM=0;
+        for(int i=1;i<=n;i++){
+           if(i%m!=0) sumN+=i; 
+           else sumM+=i;
+        }
+        return (sumN-sumM);   
+    }
+}
