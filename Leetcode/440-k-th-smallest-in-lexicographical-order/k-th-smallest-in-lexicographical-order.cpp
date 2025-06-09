65a90f7444a6d8f644d16ee980375a5c068cca5d
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-06-09 20:07:19 +0600

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.26%) - LeetSync

---
+++ Leetcode/440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        long curr = 1;
+        k--; 
+        
+        while (k > 0) {
+            long steps = countSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                curr += 1;    
+                k -= steps;
+            } else {
+                curr *= 10;    
+                k -= 1;       
+            }
+        }
+        
+        return curr;
+    }
+
+    long countSteps(long n, long curr, long next) {
+        long steps = 0;
+        while (curr <= n) {
+            steps += min(n + 1, next) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+        return steps;
+    }
+};
