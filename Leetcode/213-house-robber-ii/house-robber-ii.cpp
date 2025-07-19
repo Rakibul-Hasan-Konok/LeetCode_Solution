59a8e2147c1858474b414d944b4dae0c507ef05b
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-07-19 19:30:09 +0600

    Time: 3 ms (7.45%) | Memory: 10.9 MB (20.71%) - LeetSync

--- Leetcode/213-house-robber-ii/house-robber-ii.cpp
+++ Leetcode/213-house-robber-ii/house-robber-ii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int robLinear(vector<int>& nums, int start, int end) {
-        int prev = 0, curr = 0;
-        for (int i = start; i <= end; ++i) {
-            int temp = max(curr, prev + nums[i]);
-            prev = curr;
-            curr = temp;
+    int rob(vector<int>& n) {
+        int ns = n.size();
+        if (ns == 1) return n[0];
+        if (ns == 2) return max(n[0], n[1]);
+        if (ns == 3) {
+            return max({n[0], n[1], n[2]});
         }
-        return curr;
-    }
 
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        return max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+        vector<long long> dp(ns + 1, 0);
+        long long mx_first_arr = 0, mx_last_arr = 0;
+
+        
+        dp[0] = n[0];
+        dp[1] = max((long long)n[0], (long long)n[1]);
+        for (int i = 2; i < ns - 1; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + n[i]);
+        }
+        mx_first_arr = dp[ns - 2];
+
+    
+        dp[1] = n[1];
+        dp[2] = max((long long)n[1], (long long)n[2]);
+        for (int i = 3; i < ns; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + n[i]);
+        }
+        mx_last_arr = dp[ns - 1];
+
+        return max(mx_first_arr, mx_last_arr);
     }
 };
