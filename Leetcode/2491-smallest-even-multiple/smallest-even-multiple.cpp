3e408f11cf936b74b6145f8162aea466eda2cd2c
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-10-02 20:50:19 +0600

    Time: 0 ms (100.00%) | Memory: 8 MB (0.90%) - LeetSync

--- Leetcode/2491-smallest-even-multiple/smallest-even-multiple.cpp
+++ Leetcode/2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        int i=n;
-        if(n%2==0) return n;
-        else i++;
-        while(1){
-             if(i%n==0) return i;
-             i+=2;
-        }
-        return -1;
+       if(n%2==0) return n;
+       else return n*2;
     }
 };
