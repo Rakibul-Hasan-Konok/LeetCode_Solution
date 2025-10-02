cbcd09c9ac5dbb4217f7ae90834f998db2166ff0
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-10-02 21:12:45 +0600

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.91%) - LeetSync

--- Leetcode/2507-number-of-common-factors/number-of-common-factors.cpp
+++ Leetcode/2507-number-of-common-factors/number-of-common-factors.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     int commonFactors(int a, int b) {
        int count=1;
-        int mn=min(a,b);
-        for(int i=2;i<=mn;i++){
-            if(a%i==0 && b%i==0) count++;
+        vector<int> a1 ;
+    vector<int> b1 ;
+    for(int i=1;i*i<=a;i++){
+        if(a%i==0) {
+            a1.push_back(i);
+            if((a/i)!=i) a1.push_back(a/i);
         }
-        return count;
-
+    }
+      for(int i=1;i*i<=b;i++){
+        if(b%i==0) {
+            b1.push_back(i);
+            if((b/i)!=i) b1.push_back(b/i);
+        }
+    }
+    sort(a1.begin(), a1.end());
+    sort(b1.begin(), b1.end());
+    
+    vector<int> common;
+    set_intersection(a1.begin(), a1.end(), b1.begin(), b1.end(), back_inserter(common));
+     
+     return common.size();
 
     }
 };
